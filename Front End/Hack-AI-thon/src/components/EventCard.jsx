a65e80e95e1ce66d24423c21cdc97f7ae10fd945
unknown <vetcherala@gmail.com>
2025-01-02 17:59:28 +0530

    front page edited

--- Front End/Hack-AI-thon/src/components/EventCard.jsx
+++ Front End/Hack-AI-thon/src/components/EventCard.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import img2 from "../assets/images/hybrid.png";
+import img3 from "../assets/images/free.png";
+import img4 from "../assets/images/team.png";
 import "./EventCard.css"; // External CSS file for styling
 
 const EventCard = () => {
@@ -9,15 +12,24 @@ const EventCard = () => {
       </div>
       <div className="event-details">
         <div>
-          <h2>HACK-AI-THON 2024</h2>
+          <h1>HACK-AI-THON 2024</h1>
           <div className="event-info">
-            <span className="tag hybrid">Hybrid</span>
-            <span className="tag free">Free</span>
-            <span className="tag team-size">Team Size: 1-4</span>
+            <span className="tag hybrid">
+              <img src={img2} alt="Hybrid" className="tag-icon" />
+              Hybrid
+            </span>
+            <span className="tag free">
+              <img src={img3} alt="Free" className="tag-icon" />
+              Free
+            </span>
+            <span className="tag team-size">
+              <img src={img4} alt="Team Size: 1-4" className="tag-icon" />
+              Team Size: 1-4
+            </span>
           </div>
         </div>
         <button className="register-button" disabled>
-          Registration Closed
+          Register
         </button>
       </div>
     </div>
@@ -25,3 +37,4 @@ const EventCard = () => {
 };
 
 export default EventCard;
+
